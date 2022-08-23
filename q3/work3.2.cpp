81c67f784146c8c04a426cb4cad84b133330d1a5
thanpisitsu <110213929+thanpisitsu@users.noreply.github.com>
2022-08-23 12:35:18 +0700

    Update work3.2.cpp

--- q3/work3.2.cpp
+++ q3/work3.2.cpp
@@ -6,6 +6,9 @@ int main()
 	for (i=1;i<=n*n;i++)
 	{
 		if(i%2==1)printf("*");else printf(" ");
-		if(i%n==0)printf("\n");
+		if(n%2==0&&i%n==0){
+			if((i/n)%2==1)printf("\n ");else printf("\n");
+		}
+		else if (i%n==0) printf("\n");
 	}
 }
