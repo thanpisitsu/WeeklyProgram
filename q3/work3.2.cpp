b235db32beb49a5e74cf3733c749f8a2cb82d3f5
thanpisitsu <110213929+thanpisitsu@users.noreply.github.com>
2022-09-06 10:15:17 +0700

    Update work3.2.cpp

--- q3/work3.2.cpp
+++ q3/work3.2.cpp
@@ -5,7 +5,8 @@ int main()
 	scanf("%d",&n);
 	for (i=1;i<=n*n;i++)
 	{
-		if(i%2==1)printf("*");else printf(" ");
+		if(i%2==1)printf("*");
+		else printf(" ");
 		if(n%2==0&&i%n==0){
 			if((i/n)%2==1)printf("\n ");else printf("\n");
 		}
